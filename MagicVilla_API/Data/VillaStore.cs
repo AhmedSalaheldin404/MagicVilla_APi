630c355214a31c3a01f99d6bef1c67da2a157ccf
ahmed <salaheldinahmed2024@gmail.com>
2024-12-11 12:27:54 +0200

    add villa stroe

---
+++ MagicVilla_API/Data/VillaStore.cs
@@ -0,0 +1,9 @@
+﻿using MagicVilla_API.Models.Dto;
+
+namespace MagicVilla_API.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDto> villalist = new List<VillaDto>() { new VillaDto { Id = 1, Name = "Pool View" }, new VillaDto { Id = 2, Name = "Beach View" } };
+    }
+}
