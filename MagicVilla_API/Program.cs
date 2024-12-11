ef238e218370931baafcdb1f76159cd2058d21ac
ahmed <salaheldinahmed2024@gmail.com>
2024-12-11 17:29:35 +0200

    add a connection string

--- MagicVilla_API/Program.cs
+++ MagicVilla_API/Program.cs
@@ -1,6 +1,11 @@
+using MagicVilla_API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(option => { option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSqlConnection"));
+});
 
 builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
