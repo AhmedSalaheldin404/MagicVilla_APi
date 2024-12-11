2890b9a2f8b11af68c484be627425c2be1357602
ahmed <salaheldinahmed2024@gmail.com>
2024-12-11 12:22:54 +0200

    Create model and dto villa

---
+++ MagicVilla_API/Controllers/VillaApiController.cs
@@ -0,0 +1,18 @@
+﻿using MagicVilla_API.Models;
+using MagicVilla_API.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaApiController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDto> GetVillas()
+        {
+            return new List<VillaDto> { new VillaDto { Id = 1, Name = "Pool View" }, new VillaDto { Id = 2, Name = "Beach View" } };
+        }
+    }
+}
